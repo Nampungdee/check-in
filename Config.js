f29d303a719ff824601a7ff0e97d2eef09717312
Nampungdee <Nampungdee5555@gmail.com>
2025-07-14 22:29:59 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxIBYKUsNXnUeMaKfn5CuVa28ydyoCm-mWzTRinWEF6CvgfoHD2rmO0c1Gfr2F3t0O--Q/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxQpkw9tnyOasjapXnrGa6ogIN1tbeEu3N0QS-Ix6yXgOzJ5wlAkIYyG0rW8hUgNwqW/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycby3eEBFJG2EtJTDRaK-W_aBNMOn9nYEURtibmfPMthZVBjWi7sY_YT1i2QUj-cTnXC4hQ/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxG26hCIw8P_w11DZkucLkA_ym32XSZLd5qThE4s2Kha3S8a6IKD9ran5nGill65Nwx/exec';
 //LIFF_ID
     const LIFF_ID = '2007747842-OGeyrGkv'; // LIFF ID Checkin
     const LIFF_HISTORY = '2007747842-p0YX1oBn'; // LIFF ID history
