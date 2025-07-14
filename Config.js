d594565662ea3d0cb68b4efd698f18ce88b718cc
Nampungdee <Nampungdee5555@gmail.com>
2025-07-14 21:51:42 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,6 +2,6 @@
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxKtQXKqAOxIOb8Pgh6Gg3h6-YHj8WnPTC0Lhl-Gq_aFSfWXoxfMRSo2vKFY7pa8yP6EQ/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwLkcQkCQppkiiGRrGT6yN6O4KPbPaJAeOlgtv3u6yGh2aF0ubVNgIi4oDIyslajxUj/exec';
 //LIFF_ID
-    const LIFF_ID = '2006740306-qp2xNvgM'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2006740306-qLWyP3w6'; // LIFF ID history
-    const LIFF_REGISTER = '2006740306-qlr7J9go'; // LIFF ID register
+    const LIFF_ID = '2007747842-OGeyrGkv'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2007747842-p0YX1oBn'; // LIFF ID history
+    const LIFF_REGISTER = '2007747842-oJr1GXK2'; // LIFF ID register
