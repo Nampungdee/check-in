667cca27594ca3147b377592ea17ea7568b050c0
น้ำผึ้ง <Nampungdee5555@gmail.com>
2026-06-19 14:54:54 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,7 +2,7 @@
 const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
 
 // *** วางลิงก์ที่คุณเพิ่งก๊อปปี้มาจากหน้า Google Apps Script (ตอนกด Deploy) ตรงนี้นะคะ ***
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzF18wFW4GkJAEFX27MSj_And_NPQhJ0t5V0H-UWcrN0zzP0Ffng1A6A6De-AAMjKyZ/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzVAB6GpNio6ZZO9M8hi3K0sQboBvsjxIEPs9BPSLJTmjsMNlUcnlbw8gXoQUJBeQ5B/exec';
 
 // LIFF_ID
 const LIFF_ID = '2007747842-OGeyrGkv';      // LIFF ID Checkin
