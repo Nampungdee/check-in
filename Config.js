8e26010ea2e73757752742bd4f6b085e1427ddfd
น้ำผึ้ง <Nampungdee5555@gmail.com>
2026-06-19 22:40:46 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,14 @@
-// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App (สำหรับระบบสมาชิกรวม)
 const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
 
-// *** วางลิงก์ที่คุณเพิ่งก๊อปปี้มาจากหน้า Google Apps Script (ตอนกด Deploy) ตรงนี้นะคะ ***
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz--5zYc0agdFi6d5x1bqDmoTo2pm_MNLF4-hmA8Ng8Qr4G7XUBvKKwetU8S38FB03v/exec';
+// URL สำหรับระบบฝั่ง CheckIn เดิม (เก็บสำรองไว้)
+const WEB_APP_CHECKIN_URL = 'https://script.google.com/macros/s/AKfycbz--5zYc0agdFi6d5x1bqDmoTo2pm_MNLF4-hmA8Ng8Qr4G7XUBvKKwetU8S38FB03v/exec';
 
-// LIFF_ID
-const LIFF_ID = '2007747842-OGeyrGkv';      // LIFF ID Checkin
-const LIFF_HISTORY = '2007747842-p0YX1oBn'; // LIFF ID history
+// *** ตัวแปรหลักที่ Report.html เรียกใช้ ***
+// นำ URL Web App ตัวใหม่ที่ได้จากการ Deploy โค้ด Apps Script (ชุดที่มี DataCheckOut) มาวางตรงนี้
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwN_dP6AshriiNt9auC1LKD-oCgdRKbpwNqcGpJa13YpXDPhoaNOCMw2jyp776U6uNysQ/exec';
+
+// LIFF_ID ต่างๆ
+const LIFF_ID = '2007747842-OGeyrGkv';       // LIFF ID Checkin
+const LIFF_HISTORY = '2007747842-p0YX1oBn';  // LIFF ID history
 const LIFF_REGISTER = '2007747842-oJr1GXK2'; // LIFF ID register
