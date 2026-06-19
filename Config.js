6f4ffcca37f97d489b8862dfdf7fe1d3935c4889
น้ำผึ้ง <Nampungdee5555@gmail.com>
2026-06-19 11:43:37 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
+const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxgVG-Kw4J7lCaSl58zptyX0phXMQ3UvIduY7Z3cAS0MHE5B0vkIB6Ri_giUPWglEgW/exec';
 
 // *** วางลิงก์ที่คุณเพิ่งก๊อปปี้มาจากหน้า Google Apps Script (ตอนกด Deploy) ตรงนี้นะคะ ***
 const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzaONWU5G5smMlmnNIpN5q82z_N_iQOvYHkvQcKZUqov0G-6BwCDZRoBTa02mrdp-A3/exec';
