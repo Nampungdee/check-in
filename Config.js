8550f2c10eb9e24cb0611b57352402dff276acf6
น้ำผึ้ง <Nampungdee5555@gmail.com>
2026-06-21 01:20:50 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
-// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App (สำหรับระบบสมาชิกรวม member)
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
 const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycby2cit0L6OOwnlWYwOu9Er--MDwEHqcJUnKUBdfNrHpIwfNepA8r01x11p9Q8NGwwQ1/exec';
 
-// URL สำหรับระบบฝั่ง CheckIn เดิม (เก็บสำรองไว้)
-const WEB_APP_CHECKIN_URL = 'https://script.google.com/macros/s/AKfycbwVJ_ufylTzGiE_kyEjRO0LlBg7hrDFNxbRbYJWmVQkwF_xafq2grX4TRsfamlzEc5S/exec';
+// *** วางลิงก์ที่คุณเพิ่งก๊อปปี้มาจากหน้า Google Apps Script (ตอนกด Deploy) ตรงนี้นะคะ ***
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwVJ_ufylTzGiE_kyEjRO0LlBg7hrDFNxbRbYJWmVQkwF_xafq2grX4TRsfamlzEc5S/exec';
 
-// LIFF_ID ต่างๆ
-const LIFF_ID = '2007747842-OGeyrGkv';       // LIFF ID Checkin
-const LIFF_HISTORY = '2007747842-p0YX1oBn';  // LIFF ID history
+// LIFF_ID
+const LIFF_ID = '2007747842-OGeyrGkv';      // LIFF ID Checkin
+const LIFF_HISTORY = '2007747842-p0YX1oBn'; // LIFF ID history
 const LIFF_REGISTER = '2007747842-oJr1GXK2'; // LIFF ID register
