13902062a056aa05aa962efb4afa1ce2208fe6d0
น้ำผึ้ง <Nampungdee5555@gmail.com>
2026-06-20 16:20:16 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,6 +4,10 @@ const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycby2cit0L6OOw
 // URL สำหรับระบบฝั่ง CheckIn เดิม (เก็บสำรองไว้)
 const WEB_APP_CHECKIN_URL = 'https://script.google.com/macros/s/AKfycbyHh_6UH5lGqIes_XIz08EAWMUHpcoj0-h3Rd8BSzCp5cP9iWnH9bHwaKZiWoVa2klZ/exec';
 
+// *** ตัวแปรหลักที่ Report.html เรียกใช้ ***
+// นำ URL Web App ตัวใหม่ที่ได้จากการ Deploy โค้ด Apps Script (ชุดที่มี DataCheckOut) มาวางตรงนี้
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwN_dP6AshriiNt9auC1LKD-oCgdRKbpwNqcGpJa13YpXDPhoaNOCMw2jyp776U6uNysQ/exec';
+
 // LIFF_ID ต่างๆ
 const LIFF_ID = '2007747842-OGeyrGkv';       // LIFF ID Checkin
 const LIFF_HISTORY = '2007747842-p0YX1oBn';  // LIFF ID history
